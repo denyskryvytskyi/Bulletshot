91677cec904e6d922c40ba8bda75cbeb4891e3ef
Denys Kryvytskyi <kryvytskyi.denys@gmail.com>
2020-12-10 15:22:58 +0200

    Improved multithreading

--- Bulletshot/src/Game/BulletManager.cpp
+++ Bulletshot/src/Game/BulletManager.cpp
@@ -2,6 +2,11 @@
 #include "BulletManager.h"
 #include "Game.h"
 
+BulletManager::BulletManager()
+    : m_FailAttemptsUpdateWithSharedCount(0), m_LimitFailsCount(5)
+{
+}
+
 BulletManager::~BulletManager()
 {
     for (int32_t i = 0; i < m_BulletObjects.size(); ++i)
@@ -20,13 +25,19 @@ void BulletManager::Update(float dt)
 {
     if (m_Mutex.try_lock())
     {
-        // update bullets with shared resource
+        // Update bullets by shared resource
         if (!m_SharedResource.empty())
         {
             m_BulletObjects.insert(m_BulletObjects.end(), m_SharedResource.begin(), m_SharedResource.end());
             m_SharedResource.clear();
         }
+        m_FailAttemptsUpdateWithSharedCount = 0;
         m_Mutex.unlock();
+        m_ForceUpdateCV.notify_all(); // notify other threads to continue fire bullets
+    }
+    else
+    {
+        ++m_FailAttemptsUpdateWithSharedCount;
     }
 
     for (auto bulletIterator = m_BulletObjects.begin(); bulletIterator != m_BulletObjects.end();)
@@ -34,7 +45,7 @@ void BulletManager::Update(float dt)
         if ((*bulletIterator)->IsDestroyed())
         {
             Game::OnGameobjectDestroyed(*bulletIterator);
-            delete *bulletIterator;
+            delete* bulletIterator;
             bulletIterator = m_BulletObjects.erase(bulletIterator);
         }
         else
@@ -49,7 +60,11 @@ void BulletManager::Fire(gdm::vec2 pos, gdm::vec2 dir, float speed, float time, 
 {
     BulletObject* bullet = new BulletObject(pos, dir, speed, time, lifetime);
 
-    std::lock_guard<std::mutex> lock(m_Mutex);
+    std::unique_lock<std::mutex> lock(m_Mutex);
+
+    // wait (give access to the main thread) if reached limit of faild attempts to update bullets container by shared resource container
+    m_ForceUpdateCV.wait(lock, [this] { return !(m_FailAttemptsUpdateWithSharedCount >= m_LimitFailsCount); });
+
     m_SharedResource.emplace_back(bullet);
 }
 
--- Bulletshot/src/Game/BulletManager.h
+++ Bulletshot/src/Game/BulletManager.h
@@ -6,16 +6,29 @@
 class BulletManager
 {
 public:
-    BulletManager() = default;
+    BulletManager();
     ~BulletManager();
 
+    // Called only from main thread
     void Update(float dt);
+
+    // Called from different threads
     void Fire(gdm::vec2 pos, gdm::vec2 dir, float speed, float time, float lifetime);
 
     const std::vector<BulletObject*>& GetBulletObjects() const;
 
 private:
+
+    // Access only from main thread
     std::vector<BulletObject*> m_BulletObjects;
+
+    // Access from different threads
     std::vector<BulletObject*> m_SharedResource;
+
     std::mutex m_Mutex;
+    std::condition_variable m_ForceUpdateCV;
+
+    // Count of failed attempt to access shared resource from main thread (in update function)
+    int32_t m_FailAttemptsUpdateWithSharedCount;
+    const int32_t m_LimitFailsCount;
 };
